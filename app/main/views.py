d764706fb022e2e7bd001ebe1fa63baefd0b5a42
Arseniy Borodulin <senya.borodulin@mail.ru>
2023-10-14 17:30:16 +0300

    api

--- app/main/views.py
+++ app/main/views.py
@@ -11,3 +11,14 @@ class CityDetail(DetailView):
     model = main.models.City
     context_object_name = "city"
     template_name = "main/city_detail.html"
+
+
+class GetCities(TemplateView):
+    template_name = "main/api.html"
+
+    def get_context_data(self, **kwargs):
+        context = {
+            "data": list(main.models.City.objects.points()),
+        }
+        return context
+
