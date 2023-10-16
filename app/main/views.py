dca3014dad8b358ea9768503d26c0cc9fec97ffd
m3tr0 <mar2017ar@gmail.com>
2023-10-17 02:01:21 +0300

    detail view page

--- app/main/views.py
+++ app/main/views.py
@@ -4,19 +4,18 @@ from django.http import JsonResponse
 import main.models
 
 
-class HelloWorld(TemplateView):
+class MainPage(TemplateView):
     template_name = 'main/main.html'
 
 
 class CityDetail(DetailView):
     model = main.models.City
     context_object_name = "city"
-    template_name = "main/city_detail.html"
+    template_name = "main/city.html"
 
 
 class GetCities(View):
     template_name = "main/api.html"
-
 
     def get(self, request):
         return JsonResponse({
